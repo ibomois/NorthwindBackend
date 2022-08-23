817e7c1fbc9defe36f4ffeb240ffdfb3e73d6834
Dadaş <Dada@DESKTOP-BQT9OT7>
2022-08-23 22:08:53 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity 
+    {
+        public int ProtectId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string QuantityPerUnit  { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public short UnitInStock { get; set; }
+
+
+    }
+}
